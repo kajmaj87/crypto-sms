e921e01f5b7127ed03e774fa47065252ea828a1a
kajman <kajmaj87@gmail.com>
2021-10-23 10:40:28 +0200

    Cleanup, merged endpoints to one

--- api/cert.js
+++ api/cert.js
@@ -1,14 +1,18 @@
+
+import axios from "axios";
+
 const certList = require('../resources/certList.json');
+const url = 'https://hole.cert.pl/domains/domains.json';
 
 const handler = async (event) => {
     if (event.httpMethod === 'POST') {
         try {
             const request = JSON.parse(event.body);
             let certDomainList = JSON.parse(JSON.stringify(certList)).map(domain => domain.DomainAddress);
-            const isLinkInMessage = certDomainList.find(domain => request.text.includes(domain));
+            const isLinkInMessage = !!certDomainList.find(domain => request.text.includes(domain));
             return {
                 statusCode: 200,
-                body: JSON.stringify({ "message_valid": !!isLinkInMessage})
+                body: JSON.stringify({ "contains_malicious_link": isLinkInMessage})
             }
         } catch (error) {
             return {
@@ -17,6 +21,13 @@ const handler = async (event) => {
             }
         }
 
+    }
+    if (event.httpMethod === 'GET'){
+        const response = await axios.get(url);
+            return {
+                statusCode: response.status,
+                body: JSON.stringify(response.data)
+            }
     }
 }
 
